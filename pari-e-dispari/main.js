9f5896b382ef80e3496d79b6b6148cf1a9f5591e
giuliatognali <giuliatognali@gmail.com>
2023-03-01 16:09:29 +0100

    somma numeri

--- pari-e-dispari/main.js
+++ pari-e-dispari/main.js
@@ -1,19 +1,24 @@
 'use strict';
 
 
-function sommaNumeri(numUtente, numCpu){
+
+//variabile numero utente
+const numUtente = Number(prompt('Inserisci un numero da 1 a 5'));  //numero scelto dall'utente
+console.log(`numero utente ${numUtente}`);
+
+//funzione numero random
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + 1;
+}
+//argomenti funzione tra 1 e 6
+const numCpu = randomNum(1, 6);
+console.log(`numero computer ${numCpu}`);
+
+function sommaNumeri(numUtente, numCpu) {
     const somma = numUtente + numCpu;
     return somma;
     //alert(somma);
 }
-
-//variabile numero utente
-const numUtente = Number(prompt('Inserisci un numero da 1 a 5'));  //numero scelto dall'utente
-console.log(numUtente);
-
-//
-const numCpu = 7;
-
-const somma = sommaNumeri(numUtente, 7);
-console.log(somma);
+const somma = sommaNumeri(numUtente, numCpu);
+console.log(`${somma} è la somma dei due numeri `);
 
